ce15f2b2984111a388e88eda3ae7df5b97be7127
deguet <joris.deguet@gmail.com>
2026-05-16 08:26:53 -0400

    signatures des fonctions

---
+++ racine.py
@@ -0,0 +1,17 @@
+
+
+def racineArrondiDicho(nombre, decimales = 5):
+  # ici on va programmer la racine du nombre arrondi à un certain nombre de décimales
+  print("A faire")
+
+def racineArrondiChiffres(nombre, decimales = 5):
+  # ici on va programmer la racine du nombre arrondi à un certain nombre de décimales
+  print("A faire")
+
+def racineN(nombre, exposant=2,  decimales = 5):
+  # ici on va programmer la racine du nombre arrondi à un certain nombre de décimales
+  print("A faire")
+
+def racine(nombre):
+  # la racine carrée la plus précise possible, réflexion sur quand arrêter ton algo
+  print("A faire")
