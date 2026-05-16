304d43b68a068d8a48b756b926da07a00f1fd258
deguet <joris.deguet@gmail.com>
2026-05-16 08:32:32 -0400

    base de fichier principal pour travailler

---
+++ test.py
@@ -0,0 +1,10 @@
+from racine import *
+# A compléter par vous, changez, ajoutez des cas pour pouvoir tester souvent votre code.
+
+# un premier test la racine de 9 devrait être 3
+resultat = dicho_arrondie(3)
+print(resultat)
+print(" réussite ? ", resultat == 3)
+
+#print(dicho_arrondie(2, 5), " devrait être proche de ", 1.41421, " réussite ? ", dicho_arrondie(2, 5) == 1.41421) # la racine de 2 1.41421 avec 5 décimales
+#print(dicho_arrondie(2, 4), " devrait être proche de ", 1.4142, " réussite ? ", dicho_arrondie(2, 5) == 1.4142)  # la racine de 2 1.4142 avec 4
