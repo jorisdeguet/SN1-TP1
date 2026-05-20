a4d17dac2907e5854dd3333deab91fe999cccb95
joris deguet <joris.deguet@gmail.com>
2026-05-20 07:25:44 -0400

    tagged

--- performance.py
+++ performance.py
@@ -3,3 +3,4 @@ from racine import *
 # Dans ce fichier vous appellerez les fonctions du module racine pour comparer leurs performances.
 
 test = racine_arrondi_dicho(56984657)
+				 	 		  		       	      	  		  				   		  		   	 	 	     
